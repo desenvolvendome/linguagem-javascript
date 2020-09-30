b0bf3bf90f4b99700412e4737829d28030b34522
Cauê Santos <caah.sfc@live.com>
2020-09-30 09:40:46 -0300

    Corrigindo indentação

--- Caue-Santos/linguagem-javascript/Fatorial.js
+++ Caue-Santos/linguagem-javascript/Fatorial.js
@@ -1,17 +1,16 @@
-class Fatorial{
-	
-	static calcula(){
-		console.log("~~~~~ Fatorial ~~~~~");
+class Fatorial{	
+  static calcula(){
+    console.log("~~~~~ Fatorial ~~~~~");
+    let numero = 5;
+    let fatorial = 1;
 
-		let numero = 5;
-		let fatorial = 1;
+    for(let i = 1; i <= numero; i++){
+      fatorial *= i;
+    }
 
-		for(let i = 1; i <= numero; i++){
-			fatorial *= i;
-		}
-
-		console.log(fatorial);
+  console.log(fatorial);	  
   }
+	
 }
 
 module.exports = Fatorial;
