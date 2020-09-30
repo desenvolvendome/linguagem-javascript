f63af7c74031cd349b9292cca6904ef1e34d5e1b
Cauê Santos <caah.sfc@live.com>
2020-09-30 12:24:08 +0000

    Criando o algoritmo para calcular o fatorial de um número e chamando na main

--- Caue-Santos/linguagem-javascript/Fatorial.js
+++ Caue-Santos/linguagem-javascript/Fatorial.js
@@ -1,3 +1,17 @@
-class Fatorial{}
+class Fatorial{
+	
+	static calcula(){
+		console.log("~~~~~ Fatorial ~~~~~");
+
+		let numero = 5;
+		let fatorial = 1;
+
+		for(let i = 1; i <= numero; i++){
+			fatorial *= i;
+		}
+
+		console.log(fatorial);
+  }
+}
 
 module.exports = Fatorial;
--- Caue-Santos/linguagem-javascript/Main.js
+++ Caue-Santos/linguagem-javascript/Main.js
@@ -1,3 +1,5 @@
 const fatorial = require('./Fatorial.js');
 const linearSearch = require('./LinearSearch.js');
 const bubbleSort = require('./BubbleSort.js');
+
+fatorial.calcula();
