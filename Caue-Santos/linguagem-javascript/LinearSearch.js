3caaccabe3e63c35d77b38bb33cae4596c8f127d
Cauê Santos <caah.sfc@live.com>
2020-09-30 09:42:17 -0300

    Corrigindo indentação

--- Caue-Santos/linguagem-javascript/LinearSearch.js
+++ Caue-Santos/linguagem-javascript/LinearSearch.js
@@ -1,21 +1,21 @@
 class LinearSearch{
+  static calcula(){
+  console.log("~~~~~ LinearSearch ~~~~~");
 
-	static calcula(){
-		console.log("~~~~~ LinearSearch ~~~~~");
+  let elemento = 11;
+  let array = [25, 104, 10, 88, 74, 99, 26, 11, 3, 0];
+  let indice = -1;
 
-		let elemento = 11;
-		let array = [25, 104, 10, 88, 74, 99, 26, 11, 3, 0];
-		let indice = -1;
+  for(let i=0;i<array.length;i++){    
+    if(array[i] == elemento){    
+      indice = i;
+    }    
+  }   
+ 
+ console.log("Indíce do elemento desejado: " + indice);
 
-		for(let i=0;i<array.length;i++){    
-			if(array[i] == elemento){    
-				indice = i;
-			}    
-		}   
-
-		console.log("Indíce do elemento desejado: " + indice);
-
-	}       
+ }   
+	
 }
 
 module.exports = LinearSearch;
